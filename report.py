2fbc96f52f2a3dd56a7979ca60fcd74d39055633
Joppe Vos <>
2019-07-09 08:44:37 +0200

    making changes in recieving data

--- report.py
+++ report.py
@@ -4,8 +4,108 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from seaborn import load_dataset
 from IPython.core.display import display, HTML
+import os
 
-df = load_dataset('titanic')
+class Edas:
+
+    def __init__(self, data):
+        if isinstance(dataframe, pd.DatFrame):
+            self.df = data
+        elif isintance(data, str):
+            _, file_extension = os.path.splitext(data)
+            if file_extension == ('.csv'):
+                self.df = pd.read_csv(path_csv)
+            elif file_extension == ('.exc'):
+                self.df = pd.read_excel(path_csv)
+        else:
+            "Unable to open file"
+
+        self.df = pd.read_csv(path_csv)
+        self.write_html_report()
+        self.display_html_report()
+
+    def display_html_report(self):
+        """ display the written hml"""
+        display(HTML('report_page.html'))
+
+    def write_html_report(self):
+        html_page = self.create_html_layout()
+        with open("report_page.html", "w") as html_file:
+            html_file.write(html_page)
+            html_file.close()
+
+    def create_html_layout(self):
+        """ create html page to render out
+        :param df: dataframe
+        """
+        page = """<!DOCTYPE html>
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          </head>
+        </html>
+        <head>
+        	<meta charset="UTF-8">
+        </head>
+        <body>
+          <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <h4>Edas report: Exploratory data analysis</h4>
+            </div>
+            <div class="col-sm">
+              <h3>Inspecting dataframe of size: {size}
+            </div>
+            </div>
+          </div>
+        	<table class="table table-hover" style=".table">
+              <thead>
+                <tr style="font-size: 15px;">
+                  <th width="5%" align="left" scope="col">Variable Name</th>
+                  <th width="12%" align="left"  scope="col">Data Type</th>
+                  <th width="15%" align="left"  scope="col">Histogram</th>
+                  <th width="11%" align="left"  scope="col">Stats</th>
+                  <th width="7%" align="left"  scope="col">Missing NA</th>
+                  <th width="5%" align="left"  scope="col">Outliers</th>
+                </tr>
+              </thead>
+              <tbody>""".format(size=self.df.size)
+
+        end_page = """  
+        </tbody>
+        </table>
+        </body>
+        """
+        rows_html = []
+        for i, column in enumerate(self.df.columns):
+            Summary = ColumnSummary(data=self.df[column])
+            datatype = Summary.data_type()
+            missing = Summary.missing_values()
+            stats = Summary.statistic_summary()
+            outliers = Summary.outliers()
+            Summary.create_histogram(i)
+            html = f"""
+            <tr>
+              <td style="font-size: 15px;" width="10%" align="left"> {column}</td>
+              <td style="font-size: 15px;"width="10%" align="left"> {datatype}</td>
+              <td><img class="img-fluid" src="hist_images/histogram{i}.png" style="width:800px"> </td>
+              <td style="font-size: 15px;">mean: {stats.mean}<br>
+                  mode: {stats.mode}<br><br>
+                  min: {stats.min}<br>
+                  max: {stats.max}<br><br>
+                  lower-bound: {stats.lower}<br>
+                  upper-bound: {stats.upper}<b</td>
+              <td style="font-size: 15px;">{missing}</td>
+              <td style="font-size: 15px;">{outliers}</td>
+            </tr>
+            """
+            rows_html.append(html)
+
+        merged_html = page + "".join(rows_html) + end_page
+        return merged_html
+
 
 class ColumnSummary:
     def __init__(self, data):
@@ -14,8 +114,8 @@ class ColumnSummary:
     def statistic_summary(self):
         stats = namedtuple('stats', ['mean', 'mode', 'min', 'max', 'upper', 'lower'])
         if not self.dtype_is_object():
-            return stats(round(self.mean(), 2), self.mode()[0], self.min(),
-                         self.max(), round(self.q3(), 2), round(self.q1(), 2))
+            return stats(round(self.mean(), 2), round(self.mode()[0],2), round(self.min(),2),
+                         round(self.max(), 2), round(self.q3(), 2), round(self.q1(), 2))
         else:
             return stats('', self.mode()[0], '', '', '', '')
 
@@ -27,10 +127,10 @@ class ColumnSummary:
         """ return number of missing values"""
         missing_values = self.data.isna().sum()
         if missing_values:
-            fracture = missing_values/self.data.count()
+            fracture = missing_values / self.data.count()
             return f"N={missing_values},{round(fracture, 2)}%"
         else:
-            return "No missing values"
+            return "no missing values"
 
     def dtype_is_object(self):
         if self.data.dtype in ('int64', 'float64', 'int32', 'float32'):
@@ -68,7 +168,7 @@ class ColumnSummary:
         create a distribution plot from data without missing values
         :param i: iteration for each histogram
         """
-        fig, ax = plt.subplots(1, 1, figsize = (5, 5), dpi=100)
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10), dpi=100)
         sns.set(style="whitegrid")
         if self.dtype_is_object() or self.num_of_values() <= 10:
             sns.countplot(self.remove_nan_values())
@@ -77,10 +177,15 @@ class ColumnSummary:
         # styling
         ax.set_xlabel('')
         ax.set_ylabel('')
-        plt.rc('axes', labelsize=30)    # fontsize of the x and y labels
-        plt.rc('xtick', labelsize=15)    # fontsize of the tick labels
-        plt.rc('ytick', labelsize=15)
+        font = {'weight': 'bold'}
+        plt.rc('font', **font)
+        plt.rc('axes', labelsize=30)  # fontsize of the x and y labels
+        plt.rc('xtick', labelsize=30)  # fontsize of the tick labels
+        plt.rc('ytick', labelsize=30)
+        if not os.path.isdir('hist_images'):
+            os.mkdir('hist_images')
         plt.savefig(f'hist_images/histogram{i}.png', bbox_inches='tight')
+        plt.close()
 
     def remove_nan_values(self):
         return self.data.dropna().copy()
@@ -91,16 +196,16 @@ class ColumnSummary:
             data_clean = self.remove_nan_values()
             q1 = self.q1()
             q3 = self.q3()
-            iqr = self.q3()-self.q1()
+            iqr = self.q3() - self.q1()
             cut_off = iqr * 1.5
             lower, upper = q1 - cut_off, q3 + cut_off
             mask = data_clean.between(lower, upper)
             data_between = data_clean[mask]
             outliers = len(data_clean) - len(data_between)
             if outliers == 0:
-                return 'No outliers'
+                return 'no outliers'
             return outliers
-        return 'No outliers'
+        return 'no outliers'
 
     def mean(self):
         return self.data.mean()
@@ -120,88 +225,7 @@ class ColumnSummary:
     def q3(self):
         return self.data.quantile(q=0.75)
 
-def create_html_layout(df):
-    """ create html page to render out
-    :param df: dataframe
-    """
-    page = """<!DOCTYPE html>
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="custom.css">
-        <link href="custom.css" rel="stylesheet">
-      </head>
-    </html>
-    <head>
-    	<meta charset="UTF-8">
-    </head>
-    <body>
-      <div class="container">
-      <div class="row">
-        <div class="col-sm">
-          <h4>Edas report: Exploratory data analysis</h4>
-        </div>
-        <div class="col-sm">
-          <h3>Inspecting dataframe of size: {size}
-        </div>
-        <div class="col-sm">
-          One of three columns
-        </div>
-        </div>
-      </div>
-    	<table class="table table-hover table-striped" style=".table">
-          <thead>
-            <tr>
-              <th width="10%" align="left" scope="col">Variable Name</th>
-              <th width="10%" align="left"  scope="col">Data Type</th>
-              <th width="10%" align="left"  scope="col">Histogram</th>
-              <th width="10%" align="left"  scope="col">Stats</th>
-              <th width="10%" align="left"  scope="col">Missing NA</th>
-              <th width="10%" align="left"  scope="col">Outliers</th>
-            </tr>
-          </thead>
-          <tbody>""".format(size=df.size)
-    end_page = """  
-    </tbody>
-    </table>
-    </body>
-    """
-    rows_html = []
-    for i, column in enumerate(df.columns):
-        Summary = ColumnSummary(data=df[column])
-        datatype = Summary.data_type()
-        missing = Summary.missing_values()
-        stats = Summary.statistic_summary()
-        outliers = Summary.outliers()
-        Summary.create_histogram(i)
-        html = f"""
-        <tr>
-          <td width="10%" align="left"> {column}</td>
-          <td width="10%" align="left"> {datatype}</td>
-          <td><img class="img-fluid" src="hist_images/histogram{i}.png" style="width:400px"> </td>
-          <td>mean: {stats.mean}<br>
-              mode: {stats.mode}<br><br>
-              min: {stats.min}<br>
-              max: {stats.max}<br><br>
-              lower-bound: {stats.lower}<br>
-              upper-bound: {stats.upper}<br></td>
-          <td>{missing}</td>
-          <td>{outliers}</td>
-        </tr>
-        """
-        rows_html.append(html)
-        rows_html.append(html)
+# df = load_dataset('titanic')
+df = 'diamonds.csv'
 
-    merged_html = page + "".join(rows_html) + end_page
-    return merged_html
-
-
-
-with open("seenopsis_output.html", "w") as html_file:
-    html_file.write(merged_html)
-    html_file.close()
+Edas(df)
